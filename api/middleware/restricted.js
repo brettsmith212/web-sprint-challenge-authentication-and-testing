d7bd18c276607e4c85ac60ab34fb8c59db22aa31
Brett Smith <brettsmith212@gmail.com>
2022-02-24 19:38:08 -0800

    restricted added

--- api/middleware/restricted.js
+++ api/middleware/restricted.js
@@ -1,5 +1,22 @@
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET } = require("../secrets");
+
 module.exports = (req, res, next) => {
-  next();
+  const token = req.headers.authorization;
+  if (!token) {
+    res.status(401).json({ message: "token required" });
+    return;
+  } else {
+    jwt.verify(token, JWT_SECRET, (err, decodedToken) => {
+      if (err) {
+        res.status(401).json({ message: "token invalid" });
+        return;
+      } else {
+        req.decodedJwt = decodedToken;
+        next();
+      }
+    });
+  }
   /*
     IMPLEMENT
 
